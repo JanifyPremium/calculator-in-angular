d7bddf0b253e525a0becaf9011fa09fd202a8173
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-03 15:53:46 +0200

    styling

---
+++ Calculator/src/app/app.js
@@ -0,0 +1,21 @@
+
+const button = document.getElementById('result').value;
+button.addEventListener('click', function diplay(num){
+
+    let a = document.getElementById('result').value += num
+
+    return num;
+    console.log(num)
+});
+
+button.addEventListener('click',function solve() {
+    let x = document.getElementById('result').value ;
+    let y = eval(x);
+
+    document.getElementById('result').value = y;
+    return y;
+});
+
+button.addEventListener('click',function clearScreen(){
+    document.getElementById('result').value = '';
+});
