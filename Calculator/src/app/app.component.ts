8e700a59bdc48f6db586fd9dc820f89913818b5c
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-04 16:22:17 +0200

    touchups an klasse für =

--- Calculator/src/app/app.component.ts
+++ Calculator/src/app/app.component.ts
@@ -12,23 +12,22 @@ import {HomeComponent} from './home/home.component';
 export class AppComponent {
   title = 'Calculator';
   
+
 display(val: any): any {
-    // Get the HTML input element with the id 'result'
     const result: HTMLInputElement = document.getElementById('result') as HTMLInputElement;
     
-    // Append the value of 'val' to the current value of the 'value' property of the input element
     result.value += val;
     
-    // Return the value of 'val'
     return val;
 }
 
 solve(): any {
     const result: HTMLInputElement = document.getElementById('result') as HTMLInputElement;
     const x: string = result.value;
-    const y: number = eval(x);
-    result.value = y.toString();
-    return y;
+    const solvedcalculation: number = eval(x);
+    result.value = solvedcalculation.toString();
+
+    return solvedcalculation;
 }
 
 clearScreen(): any {
