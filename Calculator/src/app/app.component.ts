fa8f3ace19aaac0261de642567d675e80b414b44
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-04 15:12:51 +0200

    finished

--- Calculator/src/app/app.component.ts
+++ Calculator/src/app/app.component.ts
@@ -11,4 +11,31 @@ import {HomeComponent} from './home/home.component';
 })
 export class AppComponent {
   title = 'Calculator';
+  
+display(val: any): any {
+    // Get the HTML input element with the id 'result'
+    const result: HTMLInputElement = document.getElementById('result') as HTMLInputElement;
+    
+    // Append the value of 'val' to the current value of the 'value' property of the input element
+    result.value += val;
+    
+    // Return the value of 'val'
+    return val;
+}
+
+solve(): any {
+    const result: HTMLInputElement = document.getElementById('result') as HTMLInputElement;
+    const x: string = result.value;
+    const y: number = eval(x);
+    result.value = y.toString();
+    return y;
+}
+
+clearScreen(): any {
+
+    const result: HTMLInputElement = document.getElementById('result') as HTMLInputElement;
+    result.value = '';
+}
+
+
 }
